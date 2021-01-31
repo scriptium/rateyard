2a2cdb3957d07f0061be4539053018289b4e01d8
Denys Shevel <denys.shevel16@gmail.com>
2021-01-31 19:37:36 +0200

    Add header to front

---
+++ front/front/includes/header.php
@@ -0,0 +1,13 @@
+<div id="header">
+    <div>
+        <div>Rateyard</div>
+        <div>Admin</div>
+    </div>
+    <div>
+        <a href="users.php"><div>УЧНІ</div></a>
+        <a href="groups.php"><div>ГРУПИ</div></a>
+        <a href="teachers.php"><div>ВЧИТЕЛІ</div></a>
+        <a href="classes.php"><div>КЛАСИ</div></a>
+        <a href="subjects.php"><div>ПРЕДМЕТИ</div></a>
+    </div>
+</div>
---
+++ front/front/public/admin/users.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/static/css/base.css">
+    <link rel="stylesheet" href="/static/css/admin/base.css">
+    <link rel="stylesheet" href="/static/css/admin/header.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/../includes/header.php' ?>
+    <div id="content">
+        Hello
+    </div>
+</body>
+
+</html>
