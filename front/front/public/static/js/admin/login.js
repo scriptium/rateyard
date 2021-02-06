41f03d2148ac3265566bac2ff04dd4bd61156996
Denys Shevel <denys.shevel16@gmail.com>
2021-02-06 11:53:55 +0200

    Fix login page

--- front/front/public/static/js/admin/login.js
+++ front/front/public/static/js/admin/login.js
@@ -1,22 +1,16 @@
+checkUserData('students.php', undefined);
+
 async function loginButton(button) {
     disableButton(button)
     login(
         document.getElementById('password_input').value,
         document.getElementById('username_input').value
     ).then((responseData) => {
-        if (responseData.code == 200) {
-            parsedJSON = JSON.parse(responseData.text)
-            localStorage.setItem('api_access_token', parsedJSON.access_token)
-            localStorage.setItem('api_refresh_token', parsedJSON.refresh_token)
-            document.location.replace('students.php')
-        }
-        else {
-            console.log('wrong');
-            enableButton(button);
-        }
+        parsedJSON = JSON.parse(responseData.text)
+        localStorage.setItem('api_access_token', parsedJSON.access_token)
+        localStorage.setItem('api_refresh_token', parsedJSON.refresh_token)
+        document.location.replace('students.php')
+    }, (responseData) => {
+        if (responseData.code == 403) enableButton(button) 
     })
-}
-
-window.onload = async () => {
-    await checkUserData('students.php', undefined);
 }
