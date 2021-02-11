8a8aefa0da9c23d730b634daf048f5271fbcd92a
Denys Shevel <denys.shevel16@gmail.com>
2021-02-11 11:21:48 +0200

    Add default groups to db

--- db/init.sql
+++ db/init.sql
@@ -30,7 +30,8 @@ CREATE TABLE subjects (
 CREATE TABLE groups (
     id SERIAL PRIMARY KEY NOT NULL, 
     class_id INTEGER REFERENCES classes(id) ON DELETE CASCADE NOT NULL,
-    group_name VARCHAR(256) NOT NULL
+    group_name VARCHAR(256) NOT NULL,
+    is_deletable BOOLEAN NOT NULL
 );
 
 CREATE TABLE students_groups (
@@ -75,14 +76,22 @@ INSERT INTO classes (class_name) VALUES ('11Б');
 INSERT INTO classes (class_name) VALUES ('11В');
 INSERT INTO classes (class_name) VALUES ('11Г');
 
-INSERT INTO groups (group_name, class_id) VALUES ('Група А', 1);
-INSERT INTO groups (group_name, class_id) VALUES ('Група Б', 1);
-
-INSERT INTO groups (group_name, class_id) VALUES ('Група А', 2);
-INSERT INTO groups (group_name, class_id) VALUES ('Група Б', 2);
-
-INSERT INTO groups (group_name, class_id) VALUES ('Група А', 3);
-INSERT INTO groups (group_name, class_id) VALUES ('Група Б', 3);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 1, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 2, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 3, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 4, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 5, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 6, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 7, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 8, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 9, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 10, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 11, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 12, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 13, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 14, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 15, False);
+INSERT INTO groups (group_name, class_id, is_deletable) VALUES ('Весь клас', 16, False);
 
 INSERT INTO students (username, full_name, email, password_hash, class_id)
 VALUES ('zhmisenko1', 'Жмишенко Валерій Альбертович', 'zmyshenko1@mail.com', 'a', 1);
