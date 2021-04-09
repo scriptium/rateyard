3aceb24c2a39e8b5dd9da77b916d79cc22802a4c
Denys Shevel <denys.shevel16@gmail.com>
2021-04-09 20:14:04 +0300

    Add index page

--- front/front/public/index.php
+++ front/front/public/index.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rateyard</title>
+    <link rel="stylesheet" href="/static/css/base.css">
+    <link rel="stylesheet" href="/static/css/index.css">
 </head>
 <body>
-    Rateyard Index
+    <div>
+        <div id="content">
+            <div>
+                <div class="logo_main">Rateyard</div>
+                <div class="logo_caption">Електронний журнал</div>
+            </div>
+            <div>
+                <a class="blue_button" href="/student/login.php">Вхід для учнів</a>
+                <a class="blue_button" href="/teacher/login.php">Вхід для вчителів</a>
+            </div>
+        </div>
+        <div id="footer">
+            © 2021
+            <a href="https://github.com/scriptium">scriptium</a>
+        </div>
+    <div>
 </body>
 </html>
