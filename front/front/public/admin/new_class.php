046fa4c02b188444f5273ff2406ab2e363101585
Denys Shevel <denys.shevel16@gmail.com>
2021-09-10 13:22:03 +0300

    Change new class page

--- front/front/public/admin/new_class.php
+++ front/front/public/admin/new_class.php
@@ -15,12 +15,11 @@
     <?php
     $current = 'classes';
     require $_SERVER['DOCUMENT_ROOT'] . '/../includes/admin/header.php';
-    require $_SERVER['DOCUMENT_ROOT'] . '/../includes/preloader.php';
     ?>
     <div id="content">
         <div class="title_block">
             <div class="title">Новий клас</div>
-            <div class="appear_on_change appear_transition title_block">
+            <div class="title_block">
             </div>
             <div class="blue_button" onclick="saveNewClassButton(this)">
                 <div class="button_icon save_icon"></div>
@@ -30,28 +29,11 @@
         <div class="appear_after_classes input_grid">
             <div>Назва:</div><input id="name" type="text" class="default_input_text" placeholder="Введіть назву класу">
         </div>
-        <div class="subtitle">Додати учнів до класу</div>
-        <input type="text" class="default_input_text" placeholder="Пошук учнів" oninput="searchStudents(this.value)">
-        <table class="default_table" id="group_students">
-            <thead>
-                <tr>
-                    <th style="width: 25px;"></th>
-                    <th>№</th>
-                    <th>ПІБ</th>
-                    <th>Логін</th>
-                    <th>Адреса електронної пошти</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
     </div>
     <script type="text/javascript" src="/static/js/base.js"></script>
     <script type="text/javascript" src="/static/js/admin/base.js"></script>
     <script type="text/javascript" src="/static/js/rateyard_api_client.js"></script>
     <script type="text/javascript" src="/static/js/admin/api.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/nextapps-de/flexsearch@master/dist/flexsearch.light.js"></script>
-    <script type="text/javascript" src="/static/js/hidable_children_element.js"></script>
     <script type="text/javascript" src="/static/js/changes_set.js"></script>
     <script type="text/javascript" src="/static/js/admin/new_class.js"></script>
 </body>
--- front/front/public/static/js/admin/new_class.js
+++ front/front/public/static/js/admin/new_class.js
@@ -1,77 +1,14 @@
-let windowHasLoaded = new Promise((resolve) => { window.onload = resolve });
-
-const mainTbodyElement = document.querySelector('#group_students tbody');
-const className = document.querySelector('#name');
-
-let hidableChildrenStudentsTbody;
-let changesSet = new ChangesSet(document.querySelectorAll('.appear_on_change'));
-
-let searchIndex = FlexSearch.create({
-    encode: "icase",
-    split: /\s+/,
-    tokenize: "forward"
-});
-
-const previousURL = 'classes.php';
-
-function searchStudents(text) {
-    let searchedTrs = searchIndex.search(text);
-
-    if (text !== '') {
-        hidableChildrenStudentsTbody.hideAll();
-        searchedTrs.forEach(
-            (tr) => { hidableChildrenStudentsTbody.show(tr); }
-        );
-    }
-    else hidableChildrenStudentsTbody.showAll();
-
-    hidableChildrenStudentsTbody.update();
-}
-
-function updateClassStudentData() {
-    return new Promise(async (resolve, reject) => {
-        document.querySelectorAll('.appear_after_group_students').forEach(
-            (element) => {
-                element.classList.remove('visible');
-            }
-        );
-        let studentsData = await getStudents();
-        insertStudentsData(studentsData.json, mainTbodyElement, true, true, searchIndex);
-        hidableChildrenStudentsTbody = new HidableChildrenElement(mainTbodyElement);
-        resolve();
-    });
-}
+let classNameElement = document.getElementById('name');
 
 function saveNewClassButton(buttonElement) {
-    if (className.value === '') {
-        makeInputTextWrong(className);
-        return;
-    }
-
-    buttonElement.classList.add('disabled');
-    let name = className.value;
-    let studentsIds = [];
-
-    [...mainTbodyElement.children].forEach(tr => {
-        if (tr.firstChild.firstChild.classList.contains('checked')) {
-            studentsIds.push(+tr.children[1].innerHTML);
+    disableButton(buttonElement);
+    createClass(classNameElement.value, []).then((responseData) => {
+        if (responseData.status == 400) {
+            makeInputTextWrong(classNameElement);
+            enableButton(buttonElement);
+        }
+        else if (responseData.status == 200) {
+            location.replace('classes.php')
         }
     });
-
-    console.log(studentsIds);
-
-    createClass(name, studentsIds).then(
-        (requestData) => {
-            if (requestData.status === 200) location.replace(previousURL);
-            else {
-                makeInputTextWrong(className);
-                buttonElement.classList.remove('disabled');
-            }
-        }
-    );
 }
-
-let classStudentsHasFilled = updateClassStudentData();
-
-let mainPromise = Promise.all([classStudentsHasFilled]);
-mainPromise.then(hidePreloader);
